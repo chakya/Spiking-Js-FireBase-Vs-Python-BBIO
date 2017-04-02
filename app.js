eac8be9e1cdc241b8d6fafdff18af00ad00bcda6
Charles Wijaya <charleswijaya@Charless-MacBook-Pro-2.local>
2017-04-02 20:39:39 +0800

    add functions from asg1 and database

--- app.js
+++ app.js
@@ -36,3 +36,69 @@ var motionData= db.ref("/motionData")
 //     action:'on',
 //     time:127818271
 // });
+
+stateRef.limitToLast(50).on('child_changed', toggle);
+    
+
+
+
+function toggleLed (state) {
+  b.digitalWrite(led, state);
+}
+
+function toggleMotion(state){
+  if (state===1)// if checked activate motion detector
+  {
+    motionInterval=setInterval(checkPIR, 1000); // Checks the Sensor Every Second
+    function checkPIR(){
+    b.digitalRead(motion, printStatus);
+  }
+  
+
+    
+    function printStatus(x) 
+    {
+        if(x.value === 1){
+        console.log("Motion Detected");
+        // socket.emit("motionDetected", currentCount)
+        currentCount+=1
+    }
+            
+        else
+        {
+        console.log("No Motion Detected");
+              if (currentCount>=3)// if motion more than 3 seconds
+              {
+                 motionData.once('value', function(Snapshot) {
+                    longCount = parseInt(Snapshot.val().longMotion);
+                    motionCount = parseInt(Snapshot.val().motion);
+                    motionCount+=1;
+                    longCount+=1;
+                    motionData.update({'/longMotion':longCount, '/motion':motionCount});
+                    
+                 });
+                  seq4+='L'
+                  seq4=seq4.substr(1)
+                  console.log(seq4)
+
+                  //pass long count and motion count to be updated in client html 
+                  // socket.emit('longMotionPoll',longCount, motionCount)
+                  currentCount=0;
+              }
+              else if(currentCount>=1){// if 1<= motion<3 
+                    motionData.once('value', function(Snapshot) {
+                    shortCount = parseInt(Snapshot.val().shortMotion);
+                    motionCount = parseInt(Snapshot.val().motion);
+                    motionCount+=1;
+                    shortCount+=1;
+                    motionData.update({'/shortMotion':shortCount, '/motion':motionCount});
+                 });
+              
+                  //pass short count and motion count to be updated in client html 
+                  // socket.emit('shortMotionPoll',shortCount, motionCount)
+                  seq4+='S'
+                  seq4=seq4.substr(1)
+                  console.log(seq4)
+                  currentCount=0;
+              }
+    }
--- firebaseWebApi.js
+++ firebaseWebApi.js
@@ -14,3 +14,55 @@ this.motionDB();
 }
     // this.switchLED();
 
+
+
+  Server.prototype.initFirebase = function () {
+    this.database = firebase.database();
+    this.storage = firebase.storage();
+  };
+
+
+  // Server.prototype.loadMessages = function () {
+  //   // Reference to the /messages/ database path.
+  //   this.messagesRef = this.database.ref('motionSensorData');
+  //   // Make sure we remove all previous listeners.
+  //   this.messagesRef.off();
+//
+      // Loads the last 50 messages and listen for new ones.
+  //   var setMessage = function (data) {
+  //     var val = data.val();
+  //     this.displayMessage(val.action, val.id, val.time, val.type);
+  //   }.bind(this);
+  //   this.messagesRef.limitToLast(50).on('child_added', setMessage);
+  //   this.messagesRef.limitToLast(50).on('child_changed', setMessage);
+  // };
+Server.prototype.ledDB = function () {
+    ledState = this.database.ref('ledState');
+    // Make sure we remove all previous listeners.
+    ledState.off();
+}
+Server.prototype.motionDB = function () {
+    motionState = this.database.ref('motionState');
+    // Make sure we remove all previous listeners.
+    motionState.off();
+}
+//   Server.prototype.switchLED = function () {
+//     // Reference to the /messages/ database path.
+//     this.ledState = this.database.ref('ledState');
+//     // Make sure we remove all previous listeners.
+//     this.ledState.off();
+
+//     var setMessage = function (data) {
+//       var val = data.val();
+//     //   this.displayMessage(val.action, val.id, val.time, val.type);
+//     // }.bind(this);
+//         if (val.type=='ledToggle'){
+//             motionCount.innerText = val.type;
+//             console.log('test')
+//     }
+// console.log('wowo')  
+// }
+    // ledState.limitToLast(50).on('child_added', setMessage);
+    // ledState.limitToLast(50).on('child_changed', setMessage);
+//   ;}
+    
