7dfb2e007d346c37470477b50774b8015a395321
ckcha63 <ckcha63@student.monash.edu>
2017-04-23 19:15:42 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ var admin = require("firebase-admin");
 var b = require('bonescript');
 
 //Initialize all var needed for beaglebone
+var led = "P8_8";
+b.pinMode(led, 'out');
 var motion = "P8_7";
 var motionInterval=null;
 b.pinMode(motion, b.INPUT);
 // Fetch the service account key JSON file contents
 var serviceAccount = require("./serviceAccountKey.json");
+var currentCount=0;
+var seq4='0000'
 // Initialize the app with a service account, granting admin privileges
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -18,6 +22,21 @@ var db = admin.database();
 var stateRef = db.ref("/State"); // channel name
 var motionData= db.ref("/motionData")
 var pirRef= db.ref("/PIR")
+// ledRef.off()
+
+// ref.on("value", function(snapshot) {   //this callback will be invoked with each new object
+//   console.log(snapshot.val());         // How to retrive the new added object
+// }, function (errorObject) {             // if error
+//   console.log("The read failed: " + errorObject.code);
+// });
+
+// How to push new object
+// ref.push({
+//     id:2,
+//     type:'paradise',
+//     action:'on',
+//     time:127818271
+// });
 
 stateRef.limitToLast(50).on('child_changed', toggle);
     
@@ -41,6 +60,9 @@ function toggleMotion(state){
   if (state===1)// if checked activate motion detector
   {
     motionInterval=setInterval(checkPIR, 1000); // Checks the Sensor Every Second
+    function checkPIR(){
+    b.digitalRead(motion, printStatus);
+  }
   
 
     
