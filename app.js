24a8ff85309c791c9b0814bde4f9815c80e6c152
ckcha63 <ckcha63@student.monash.edu>
2017-04-23 19:13:27 +0800

    Clean Up JS Firebase

--- app.js
+++ app.js
@@ -2,15 +2,11 @@ var admin = require("firebase-admin");
 var b = require('bonescript');
 
 //Initialize all var needed for beaglebone
-var led = "P8_8";
-b.pinMode(led, 'out');
 var motion = "P8_7";
 var motionInterval=null;
 b.pinMode(motion, b.INPUT);
 // Fetch the service account key JSON file contents
 var serviceAccount = require("./serviceAccountKey.json");
-var currentCount=0;
-var seq4='0000'
 // Initialize the app with a service account, granting admin privileges
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -35,11 +31,6 @@ function toggle(data){
   }
 }
 
-
-function toggleLed (state) {
-  b.digitalWrite(led, state);
-}
-
 function toggleMotion(state){
   if (state===1)// if checked activate motion detector
   {
@@ -55,63 +46,13 @@ function toggleMotion(state){
         if(x.value === 1){
         console.log("Motion Detected");
         // socket.emit("motionDetected", currentCount)
-        currentCount+=1
-    }
+       }
             
         else
         {
         console.log("No Motion Detected");
-              if (currentCount>=3)// if motion more than 3 seconds
-              {
-                 motionData.once('value', function(Snapshot) {
-                    longCount = parseInt(Snapshot.val().longMotion);
-                    motionCount = parseInt(Snapshot.val().motion);
-                    motionCount+=1;
-                    longCount+=1;
-                    motionData.update({'/longMotion':longCount, '/motion':motionCount});
-                    
-                 });
-                  seq4+='L'
-                  seq4=seq4.substr(1)
-                  console.log(seq4)
-
-                  //pass long count and motion count to be updated in client html 
-                  // socket.emit('longMotionPoll',longCount, motionCount)
-                  currentCount=0;
-              }
-              else if(currentCount>=1){// if 1<= motion<3 
-                    motionData.once('value', function(Snapshot) {
-                    shortCount = parseInt(Snapshot.val().shortMotion);
-                    motionCount = parseInt(Snapshot.val().motion);
-                    motionCount+=1;
-                    shortCount+=1;
-                    motionData.update({'/shortMotion':shortCount, '/motion':motionCount});
-                 });
-              
-                  //pass short count and motion count to be updated in client html 
-                  // socket.emit('shortMotionPoll',shortCount, motionCount)
-                  seq4+='S'
-                  seq4=seq4.substr(1)
-                  console.log(seq4)
-                  currentCount=0;
-              }
+       }
     }
-if (seq4=='LSLL'){
-  console.log('Intruder Alert!!')
-   motionData.once('value', function(Snapshot) {
-    intruderCount = parseInt(Snapshot.val().intruder);
-    intruderCount+=1
-    motionData.update({'/intruder':intruderCount});
-    seq4='0000'
-  });
-}
-else{
-  console.log('no intruder')
-}
-      
-    }
-
-  }
   else
   {
       // if unchecked stop interval
